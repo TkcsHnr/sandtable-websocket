e460405263de7139df479c0340615a13052dff8f
TkcsHnr <tkcshnr@gmail.com>
2025-02-05 00:41:44 +0100

    console messages

--- server.js
+++ server.js
@@ -8,22 +8,28 @@ let webSockets = [];
 
 wss.on('connection', (ws) => {
 	if (ws.protocol === 'webapp') {
+		console.log('Webapp connected');
 		webSockets.push(ws);
 
 		ws.on('close', () => {
 			webSockets = webSockets.filter((client) => client !== ws);
+			console.log('Webapp disconnected');
 		});
 	}
 
-	if (ws.protocol === 'esp32') {
+	if (ws.protocol === 'esp') {
+		console.log('Esp connected');
 		espSocket = ws;
 
 		ws.on('close', () => {
 			espSocket = null;
+			console.log('Esp disconnected');
 		});
 	}
 
 	ws.on('message', (data) => {
+		console.log('Message received');
+
 		let message;
 		try {
 			message = JSON.parse(data);
@@ -32,16 +38,20 @@ wss.on('connection', (ws) => {
 			return;
 		}
 
-		if (ws.protocol === 'esp32') {
+		if (ws.protocol === 'esp') {
+			console.log('Sending message to webapps');
 			webSockets.forEach((webSocket) => {
 				webSocket.send(JSON.stringify(message));
 			});
 		}
 
 		if (ws.protocol === 'webapp') {
+			console.log('Sending message to esp');
 			if (espSocket) {
 				espSocket.send(JSON.stringify(message));
 			}
 		}
 	});
 });
+
+console.log('WebSocketServer listening on address: ', wss.address());
